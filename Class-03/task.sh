398f270c30ca7254de914b3e46d7aae774618af9
Anishka Nase <anishkanase@Anishkas-MacBook-Air.local>
2026-08-19 07:22:48 +0530

    Some changes

--- Class-03/task.sh
+++ Class-03/task.sh
@@ -5,11 +5,23 @@
 # print name,roll_no, comment
 ## use variables, take input, create file and directory
 
+mkdir task
+cd task 
+touch result.log
+echo "This is my result file" > result.log
+date
+echo $hostname
+echo $whoami
+df -h
+
+# read -p "Mention current data" date
+# echo "Date is $date"
 
 
-read -p "Mention current data" date
-echo "Date is $date"
+ps > process.txt
+# echo current_date=$date >> result.log - wont give date output
+echo current_date=$(date) >> result.log
+
 
 read -p "Username: " name
-echo "Name is $name"
-ps > process.txt
+echo "My name is $name" >> result.log
